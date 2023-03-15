544f9bef04aad9cac434ed872ad92203caf367eb
Alberto Marquez <albert_marquezz@hotmail.com>
2023-03-15 17:54:15 -0600

    update

---
+++ 00_Introduccion/Ejercicio_1.py
@@ -0,0 +1,35 @@
+#Ejercicios 1
+
+# Introducción
+
+print(7 + 2)                       # 9
+print(9 - 6)                       # 3
+print(8 * 4)                       # 32
+print(15 / 2)                      # 7.5
+print(4 ** 2)                      # 16
+print(10 % 2)                      # 0
+print(10 // 3)                     # 3
+print(9<11)                        # True
+print(4.7 * 6.28)                  # 29.516000000000002
+print(2j + 1 + 2j + 1)             # (2+4j)
+print('Universidad')               # Universidad
+print(['lmfao', 2, 3])             # ['lmfao', 2, 3]
+print({'tarea':'Python'})          # {'tarea': 'Python'}
+print({'agua', 7.654, 3.7})        # {3.7, 'agua', 7.654}
+print(('agua', 3.654, 3.7))        # ('agua', 3.654, 3.7)
+
+# Tipos de datos
+
+print(type(7 + 2))                   # class 'int'
+print(type(9 - 6))                   # class 'int'
+print(type(8 * 4))                   # class 'int'
+print(type(15 / 2))                  # class 'float'
+print(type(4 ** 2))                  # class 'int'
+print(type([10 % 2]))                # class 'list'
+print(type({9<11}))                  # class 'set'
+print(type({4.7 * 6.28}))            # class 'set'
+print(type((2j + 1 + 2j + 1)))       # class 'complex'
+print(type(('Universidad')))         # class 'str'
+print(type(('lmfao', 2, 3)))         # class 'set'
+print(type(('agua', 7.654, 3.7)))    # <class 'tuple'>
+print(type('agua', 3.654, 3.7))      # <class 'tuple'>
