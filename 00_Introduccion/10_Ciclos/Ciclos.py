f4d0f266b3c4c55750fb63fd53e948a9adfe585c
Alberto Marquez <albert_marquezz@hotmail.com>
2023-04-21 13:23:46 -0600

    update

---
+++ 00_Introduccion/10_Ciclos/Ciclos.py
@@ -0,0 +1,30 @@
+# numeros = [1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+# i = 0
+# for i in range(i,len(numeros)):
+#         if 1%2 == 0:
+#               print(numeros[i])
+
+
+# persona = {
+#     'nombre':'Alejandro',
+#     'apellido':'Morgan',
+#     'edad':250,
+#     'pais':'México',
+#     'skills':['JavaScript', 'React', 'Node', 'MongoDB', 'Python'],
+#     'address':{
+#         'street':'Space street',
+#         'zipcode':'02210'
+#     }
+# }
+# print ('##############################')
+# for llave in persona:
+#     if llave == 'skills':
+#        for skill in persona [ 'skills']:
+#            print(skill)
+#     break
+# else:
+#     print("no esta")
+
+
+a = 1 
+print (a)
