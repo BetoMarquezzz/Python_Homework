2d36f79d8c1b574511219eac3d9b51999b23379f
Alberto Marquez <albert_marquezz@hotmail.com>
2023-04-21 13:34:58 -0600

    update

---
+++ TAREAS/Ejercicio_ 4_condicionales.py
@@ -0,0 +1,43 @@
+#TAREA 4 CONDICIONALES 
+
+numero = 8
+
+# usar una instrucción if externa
+if (numero >= 0):
+    # inner if statement
+    if numero == 0:
+      print('el numero es cero')
+    
+    else:
+        print('el numero es positivo')
+
+else:
+    print('el numero es negativo')
+
+# El numero es positivo
+
+
+numero = - 12
+
+if numero < 0:
+    print("Numero negativo")
+
+elif numero == 0:
+    print('cero')
+else:
+    print('Numero positivo')
+
+# Numero negativo
+
+
+numero = 4
+
+if numero > 0:
+    print("Numero positivo")
+
+elif numero == 0:
+    print('cero')
+else:
+    print('Numero negativo')
+
+# Numero negativo
---
+++ TAREAS/Ejercicio_2_expresiones.py
@@ -0,0 +1,115 @@
+#                               EXPRESIONES
+
+## Expresiones aritméticas
+
+## Operador - SUMA
+a = 6 + 5             #11
+print (a)
+a = a
+b = 5
+c = a + b             #16
+print (c)            
+
+## Operador - RESTA
+a = 8 - 3             #5
+print (a)
+a = a
+b = 6
+c = b - a             #1
+print (c)
+
+## Operador - MULTIPLICACION 
+a = 5 * 3             #15
+print(a)              
+a = a
+b = 2
+c = a * b             #30
+print(c)
+
+## Operador - DIVISION 
+a = 55/7              #7.857142857142857
+print(a)
+a = a
+b = 2
+c = a / b             #3.9285714285714284
+print(c)
+
+## Operador - %
+a = 12 % 2            #0
+a = 20
+b = 4
+c = a % b             #0
+print(c)
+
+## Operador - **
+a = 3**2             #9
+print(a)
+a = 5
+b = 3
+c = a ** b           #125
+print(c)
+
+## Expresiones relacionales
+
+## Operador - <
+a = 5 < 5         #FALSE
+print(a)
+a = 3
+b = 7
+c = a < b         #TRUE
+print(c)
+
+# Operador - >
+a = 10 > 9        #TRUE
+print (a)
+a = 50
+b = 17
+c = a > b         #TRUE
+print (c)
+
+# Operador - ==
+a = 6 == 6        #TRUE
+print (a)
+a = 7
+b = 10
+c = a == b        #FALSE
+print (c)
+
+# Operador !=
+a = 5 != 4         #TRUE
+print (a)
+a = 5
+b = 6
+c = a != b         #TRUE
+print (c)
+
+# Operador - <=
+a = 6 <= 5        #FALSE
+print(a)
+a = 6
+b = 5
+c = a <= b        #FALSE
+print (c)
+
+# Operador - >=
+a = 6 >= 5         #TRUE
+print(a)
+a = 10
+b = 7
+c = a >= b         #TRUE
+print (c)
+
+# Expresiones lógicas
+
+# Operador - and
+print (5-2==3 and 6 < 3)      #  TRUE
+print (8+9 < 12 or 4==4)      #  FALSE
+
+#Operador or.
+print(4-8==2 or 23>7)         #TRUE
+print(8+7 > 9 or 2==3)        #TRUE
+
+#Operador not.
+print(not 7>6)                #FALSE
+print(not 5>8)                #TRUE
+
